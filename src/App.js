3e9741f68252e0270ac312dacef66af571f606b2
Koua <kthao@augeomarketing.com>
2022-03-08 20:00:20 -0600

    added google and apple map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { products as dataProduct } from "./utils/data";
+import AppleImage from "./assets/apple-logo.png";
+import GoogleImage from "./assets/google.png";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [filterProductCategory, setFilterProductCategory] = useState("All");
+
+  const handleclick = (lat, lng) => {
+    window.open(`http://maps.google.com/maps?t=k&q=${lat},${lng}`);
+  };
+
+  const handleClickApple = (lat, lng) => {
+    window.open(`http://maps.apple.com/?t=k&q=${lat},${lng}`);
+  };
+
+  const handleOnCategoryClicked = (event, category) => {
+    event.preventDefault();
+    setFilterProductCategory(category);
+  };
+
+  useEffect(() => {
+    // set products with data
+    setProducts(dataProduct);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="inner-wrap">
+        <div className="row">
+          <ul id="project-gallery-post-nav">
+            <li>
+              <a
+                href="!#"
+                onClick={(event) => handleOnCategoryClicked(event, "All")}
+              >
+                All Products
+              </a>
+            </li>
+            <li className="menu-item menu-item-type-post_type menu-item-object-ec_store menu-item-898">
+              <a
+                href="!#"
+                onClick={(event) => handleOnCategoryClicked(event, "Garden")}
+              >
+                Garden &amp; Lawn Soil
+              </a>
+            </li>
+            <li className="menu-item menu-item-type-post_type menu-item-object-ec_store menu-item-899">
+              <a
+                href="!#"
+                onClick={(event) => handleOnCategoryClicked(event, "Mulch")}
+              >
+                Mulch
+              </a>
+            </li>
+            <li className="menu-item menu-item-type-post_type menu-item-object-ec_store current-menu-item menu-item-900">
+              <a
+                href="!#"
+                onClick={(event) => handleOnCategoryClicked(event, "Rock")}
+              >
+                Rock &amp; Road Base
+              </a>
+            </li>
+          </ul>
+
+          <section id="content" style={{ marginTop: 50 }}>
+            <article className="post-85 page type-page status-publish hentry">
+              <section className="entry-content">
+                <section className="ec_product_page" id="ec_product_page">
+                  <ul id="ec_store_product_list">
+                    {products
+                      .filter((item) => {
+                        if (filterProductCategory === "All") {
+                          return true;
+                        }
+                        return item.category === filterProductCategory;
+                      })
+                      .map((product, index) => (
+                        <li key={index} className="ec_product_li">
+                          <div
+                            // style="padding:0px; margin:auto; vertical-align:middle;"
+                            className="ec_product_type1"
+                          >
+                            <div className="ec_image_container_none ec_dynamic_image_height">
+                              <div className="ec_product_image_display_type ec_single_grow_container ec_dynamic_image_height">
+                                <div className="ec_dynamic_image_height ec_product_image_container ">
+                                  <div className="ec_dynamic_image_height ec_product_image_1 ">
+                                    <img
+                                      src={product.productImage}
+                                      alt={product.productName}
+                                      className="skip-lazy"
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+                            <h3 className="ec_product_title_type1">
+                              {product.productName}
+                            </h3>
+
+                            {/* <div className="ec_price_container_type1">
+                      <span className="ec_price_type1">Location:</span>{" "}
+                      <span className="custom-per">{product.location}</span>
+                    </div> */}
+
+                            <div className="ec_product_addtocart_container">
+                              <span className="ec_product_addtocart ec_product_addtocart_no_margin">
+                                <a
+                                  href="!#"
+                                  onClick={() =>
+                                    handleclick(product.lat, product.lng)
+                                  }
+                                  style={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                  }}
+                                >
+                                  Navigate{" "}
+                                  <img
+                                    src={GoogleImage}
+                                    width={25}
+                                    alt="apple icon"
+                                    style={{ marginLeft: 10 }}
+                                  />
+                                </a>
+                              </span>
+                            </div>
+                            <div className="ec_product_addtocart_container">
+                              <span className="ec_product_addtocart ec_product_addtocart_no_margin">
+                                <a
+                                  href="!#"
+                                  onClick={() =>
+                                    handleClickApple(product.lat, product.lng)
+                                  }
+                                  style={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                  }}
+                                >
+                                  Navigate{" "}
+                                  <img
+                                    src={AppleImage}
+                                    width={25}
+                                    alt="apple icon"
+                                    style={{ marginLeft: 10 }}
+                                  />
+                                </a>
+                              </span>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                  </ul>
+                </section>
+              </section>
+            </article>
+          </section>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,90 @@
+export const products = [
+  {
+    productName: "Black Dirt",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2019/08/DSC_0194_Black-Dirt.jpg",
+    category: "Garden",
+    lat: "45.352462",
+    lng: "-93.566443",
+  },
+  {
+    productName: "Hardwood Mulch",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Hardwood-Mulch.jpg",
+    category: "Mulch",
+    lat: "45.350835",
+    lng: "-93.563491",
+  },
+  {
+    productName: "Red Pine Bark",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/RED_PINE_BARK_MULCH.jpg",
+    category: "Mulch",
+    lat: "45.350838",
+    lng: "-93.563477",
+  },
+  {
+    productName: "Brown Mulch",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Brown-Mulch.jpg",
+    category: "Mulch",
+    lat: "45.351093",
+    lng: "-93.563438",
+  },
+  {
+    productName: "Northern Cedar Mulch",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/NORTHERN-_CEDAR_MULCH.png",
+    category: "Mulch",
+    lat: "45.350952",
+    lng: "-93.563465",
+  },
+  {
+    productName: "Class 5 Gravel",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Class-5-Gravel-2.jpg",
+    category: "Rock",
+    lat: "45.350845",
+    lng: "-93.565033",
+  },
+  {
+    productName: "White Limestone",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2020/05/ONE-AND-A-HALF-INCH-WHITE-LIMESTONE.png",
+    category: "Rock",
+    lat: "45.350891",
+    lng: "-93.564842",
+  },
+  {
+    productName: "Minnesota Mahogany",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Minnesota-Mahogany.jpg",
+    category: "Rock",
+    lat: "45.350945",
+    lng: "-93.564613",
+  },
+  {
+    productName: "2 1/2 Slate Trap Rock",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/1222-Dresser-Trap-Rock.jpg",
+    category: "Rock",
+    lat: "45.350971",
+    lng: "-93.564468",
+  },
+  {
+    productName: "1 1/2 Slate Trap Rock",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/1-AND-A-HALF-SLATE-TRAP.png",
+    category: "Rock",
+    lat: "45.350971",
+    lng: "-93.564308",
+  },
+  {
+    productName: "3/4 Slate Trap Rock",
+    productImage:
+      "https://1k226u2zosxz37us7i2rewmc-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/3-QUARTER-INCH-SLATE-TRAP.png",
+    category: "Rock",
+    lat: "45.350990",
+    lng: "-93.564117",
+  },
+];
